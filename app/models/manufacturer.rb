14ae688b58a4d16d1b3105c992bd771268365b93
Helen Hood <helen.c.hood@gmail.com>
2014-10-07 10:56:29 -0400

    Formatting edits to models

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,7 +1,7 @@
 class Manufacturer < ActiveRecord::Base
   has_many :cars
   validates :name,
-  presence: true,
-  uniqueness: {message: "This manufacturer already exists in our database"}
+    presence: true,
+    uniqueness: {message: "This manufacturer already exists in our database"}
   validates :country, presence: true
 end
